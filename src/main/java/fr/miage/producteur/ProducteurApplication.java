2e8544080a83db8435c4ab6441fe49bb37104551
RemiSaurel <r.saurel5@gmail.com>
2023-10-06 16:44:57 +0200

    add: first commit

---
+++ src/main/java/fr/miage/producteur/ProducteurApplication.java
@@ -0,0 +1,12 @@
+package fr.miage.producteur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducteurApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProducteurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/miage/producteur/ProducteurApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.producteur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducteurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
